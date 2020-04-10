8050e28d3b83b12b34c275a213663068d68679d6
eggman79 <eggman79@criptext.com>
2020-04-10 07:14:59 +0000

    added run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+conan profile new default --detect  # Generates default profile detecting GCC and sets old ABI
+conan profile update settings.compiler.libcxx=libstdc++11 default  # Sets libcxx to C++11 ABI
+
